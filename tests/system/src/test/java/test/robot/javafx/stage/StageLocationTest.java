ee2f35867406c1287f4978e10ac7ab1d4adf97f4
Thiago Sayão <thiago.sayao@gmail.com>
2026-03-26 17:47:54 -0300

    Fix

--- tests/system/src/test/java/test/robot/javafx/stage/StageLocationTest.java
+++ tests/system/src/test/java/test/robot/javafx/stage/StageLocationTest.java
@@ -38,7 +38,6 @@ import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import test.robot.testharness.VisualTestBase;
-import test.util.Util;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
