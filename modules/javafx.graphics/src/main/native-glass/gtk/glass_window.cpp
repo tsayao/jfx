18eac28c77bf9813ea4a147f0041438c026bc889
Thiago Sayão <thiago.sayao@gmail.com>
2026-08-10 19:06:30 -0300

    Filter empty args

--- modules/javafx.graphics/src/main/native-glass/gtk/glass_window.cpp
+++ modules/javafx.graphics/src/main/native-glass/gtk/glass_window.cpp
@@ -302,7 +302,10 @@ void WindowContext::process_map() {
     LOG(LIFECYCLE, log_id, "process_map -------------------------------------------\n");
     mapped = true;
 
-    if (window_type == POPUP) return;
+    if (window_type == POPUP) {
+        notify_repaint();
+        return;
+    }
 
     // The compositor may adjust the window size and position during the process,
     // so checking again increases the chances that the final geometry matches
