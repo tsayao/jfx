15721535ada7040af6d13d47f94515ac92bab975
Thiago Sayão <thiago.sayao@gmail.com>
2026-08-15 20:33:49 -0300

    Filter empty args

--- modules/javafx.graphics/src/main/native-glass/gtk/GlassApplication.cpp
+++ modules/javafx.graphics/src/main/native-glass/gtk/GlassApplication.cpp
@@ -520,6 +520,8 @@ static void process_events(GdkEvent* event, gpointer data)
 
     EventsCounterHelper helper(ctx);
 
+    g_print("Event: %s\n", event_type_name(event->type));
+
     if ((event->type == GDK_KEY_PRESS || event->type == GDK_KEY_RELEASE) && ctx != NULL && ctx->filterIME(event)) {
         return;
     }
