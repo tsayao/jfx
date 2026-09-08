05b9dc35bcbf8a6a6943fcd8a6f37b7fa34026e1
Thiago Sayão <thiago.sayao@gmail.com>
2026-09-08 08:07:27 -0300

    Review adjustments

--- modules/javafx.graphics/src/main/native-glass/gtk/GlassApplication.cpp
+++ modules/javafx.graphics/src/main/native-glass/gtk/GlassApplication.cpp
@@ -549,7 +549,6 @@ static void process_events(GdkEvent* event, gpointer data)
                     break;
                 case GDK_MAP:
                     ctx->process_map();
-                    gtk_main_do_event(event);
                     break;
                 case GDK_UNMAP:
                 case GDK_CLIENT_EVENT:
